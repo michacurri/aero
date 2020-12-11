edae92777bccffc2295d7a634f5cd90b5ce93e8b
michacurri <mkcurri@gmail.com>
2020-12-11 13:57:30 -0500

    streamline Profile.js

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,12 +1,13 @@
 import React, {
   Fragment,
+  useContext,
   useState,
   useEffect,
   useCallback,
-  useRef,
 } from "react";
 import ProfileDisplay from "./ProfileDisplay";
 import ProfileSearchOrAdd from "../admin/ProfileSearchOrAdd";
+import { UserContext } from "../../backend/authorization/UserContext";
 
 const headers = {
   Accept: "application/json",
@@ -18,49 +19,20 @@ const headers = {
 // On Login with Google OAuth, check if profile exists in DB
 // -- load profile if it exists; else send to "add new"
 
-const Profile = ({ currentUser }) => {
+const Profile = () => {
+  const [currentUser] = useContext(UserContext);
   const [searchOrAdd, setSearchOrAdd] = useState(null);
   const [profile, setProfile] = useState({});
-  const allProfiles = useRef({});
-  const parseObject = useRef({});
 
-  const refresh = useCallback(async () => {
-    setProfile({});
+  const refreshProfile = useCallback(async () => {
     try {
-      const response = await fetch(`/profile/search/all`);
-      const profileRes = await response.json();
-      allProfiles.current = profileRes;
-    } catch (err) {
-      console.log({ err });
-    }
-  }, []);
-
-  const getProfile = useCallback(async () => {
-    try {
-      const response = await fetch("/profile/this");
-      const json = await response.json();
-      if (!response.ok) {
-        throw new Error(json.message);
-      }
-      setProfile(json.data);
-    } catch (err) {
-      console.log(err);
-      setProfile(undefined);
-    }
-  }, [setProfile]);
-
-
-  //* send user object to search and return a profile
-  //TODO - if no profile exists, set up a new one
-  const preloadQuery = useCallback(async () => {
-    try {
-      const response = await fetch(
-        `/profile/search/email/${parseObject.current.email}`,
-        {
-          method: "GET",
-          headers: headers,
-        }
-      );
+      const userEmail = currentUser.email;
+      const response = await fetch(`/profile/search/email/${userEmail}`, {
+        method: "GET",
+        headers: headers,
+        body: JSON.stringify({ email: userEmail }),
+      });
+      console.log(response);
       const profileRes = await response.json();
       setProfile(profileRes[0]);
     } catch (err) {
@@ -68,34 +40,9 @@ const Profile = ({ currentUser }) => {
     }
   }, [setProfile]);
 
-  //* handling Google Auth users
-  //* pull pertinent information from currentUser
-  const preloadParse = useCallback(async () => {
-    if (currentUser.displayName !== null) {
-      const firstLast = await currentUser.displayName;
-      const firstName = await firstLast.split(" ").shift();
-      const lastName = await firstLast.split(" ").pop();
-      const phone = await currentUser.phone;
-      const email = await currentUser.email;
-      const uid = await currentUser.uid;
-      parseObject.current = {
-        firstName,
-        lastName,
-        phone,
-        email,
-        uid,
-      };
-      const parseJson = JSON.stringify(parseObject);
-      console.log(parseJson);
-      return preloadQuery(parseJson);
-    } else return null;
-  }, [currentUser, preloadQuery]);
-
   useEffect(() => {
-    refresh();
-    getProfile()
-    preloadParse();
-  }, [refresh, getProfile, preloadParse]);
+    refreshProfile();
+  }, [refreshProfile()]);
 
   const searchProfiles = () => {
     setSearchOrAdd("search");
@@ -108,10 +55,9 @@ const Profile = ({ currentUser }) => {
     <Fragment>
       {!profile._id ? (
         <ProfileSearchOrAdd
-        // <ProfileCreate
+          // <ProfileCreate
           profile={profile}
           searchOrAdd={searchOrAdd}
-          getProfile={getProfile}
           searchProfiles={searchProfiles}
           addProfile={addProfile}
           setProfile={setProfile}

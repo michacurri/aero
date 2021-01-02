2eba40334c65461e72a90303c7d0485ef33d80ec
michacurri <mkcurri@gmail.com>
2021-01-02 12:28:10 -0500

    changed PORT to process.env.PORT

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const mongoose = require("mongoose");
 
 const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/aero";
 // const DB_URI = "mongodb://localhost:27017/aero";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+// const PORT = 5000;
 
 app.use(cookieParser());
 app.use(express.json());

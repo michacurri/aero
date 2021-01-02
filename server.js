8d12e61e40a6a8a00508716c15171586eca5edcf
michacurri <mkcurri@gmail.com>
2021-01-02 13:09:23 -0500

    heroku deploy - uncommented CORS and changed to heroku http

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 require("dotenv/config");
-// const cors = require("cors");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
